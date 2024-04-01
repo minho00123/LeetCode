72b43093971f7d612e029190abbff209fa951192
Min Ho Jang <128145017+minho00123@users.noreply.github.com>
2024-04-01 10:31:01 +0900

    Time: 340 ms (16.83%), Space: 89.4 MB (48.13%) - LeetHub

---
+++ 2623-memoize/2623-memoize.js
@@ -0,0 +1,31 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    const memory = {};
+    
+    return function(...args) {
+        const key = `${args}`;
+
+        if (key in memory) {
+            return memory[key];
+        } else {
+            memory[key] = fn(...args);
+
+            return memory[key]
+        }
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
