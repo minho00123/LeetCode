22bdef2cd8ce0569dd0cabafce7093c30e94ea62
Min Ho Jang <128145017+minho00123@users.noreply.github.com>
2024-04-20 15:52:29 +0900

    Time: 109 ms (74.47%), Space: 54.2 MB (84.52%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.js
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -2,42 +2,24 @@
  * @param {string} s
  * @return {number}
  */
+const symbol = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000,
+}
+
 var romanToInt = function(s) {
     let answer = 0;
     
     for (let i = 0; i < s.length; i++) {
-        if (s[i] === "I" && s[i + 1] && s[i + 1] === "V") {
-            answer += 4;
-            i++;
-        } else if (s[i] === "I" && s[i + 1] && s[i + 1] === "X") {
-            answer += 9;
-            i++;
-        } else if (s[i] === "X" && s[i + 1] && s[i + 1] === "L") {
-            answer += 40;
-            i++;
-        } else if (s[i] === "X" && s[i + 1] && s[i + 1] === "C") {
-            answer += 90;
-            i++;
-        } else if (s[i] === "C" && s[i + 1] && s[i + 1] === "D") {
-            answer += 400;
-            i++;
-        } else if (s[i] === "C" && s[i + 1] && s[i + 1] === "M") {
-            answer += 900;
-            i++;
-        } else if (s[i] === "I") {
-            answer += 1;
-        } else if (s[i] === "V") {
-            answer += 5;
-        } else if (s[i] === "X") {
-            answer += 10;
-        } else if (s[i] === "L") {
-            answer += 50;
-        } else if (s[i] === "C") {
-            answer += 100;
-        } else if (s[i] === "D") {
-            answer += 500;
-        } else if (s[i] === "M") {
-            answer += 1000;
+        if (symbol[s[i]] < symbol[s[i + 1]]) {
+            answer -= symbol[s[i]];
+        } else {
+            answer += symbol[s[i]];
         }
     }
     
