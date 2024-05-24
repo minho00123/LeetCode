ec588dc8898627964864e703c2abcbfe3cef1df1
Min Ho Jang <128145017+minho00123@users.noreply.github.com>
2024-05-24 09:18:33 +0900

    Time: 61 ms (65.12%), Space: 51.1 MB (33.81%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,13 +4,15 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    const answer = [];
+    const map = new Map();
     
-    for (let i = 0; i < nums.length - 1; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j]
-            }
+    for (let i = 0; i < nums.length; i++) {
+        if (map.has(target - nums[i])) {
+            return [map.get(target - nums[i]), i];
+        } else {
+            map.set(nums[i], i);
         }
     }
+
+    return [];
 };
