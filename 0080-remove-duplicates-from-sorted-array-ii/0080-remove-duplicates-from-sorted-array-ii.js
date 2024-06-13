ee0c92eab80124bea54b864efcee6049d2753145
Min Ho Jang <128145017+minho00123@users.noreply.github.com>
2024-06-13 12:40:14 +0900

    Time: 62 ms (54.70%), Space: 52 MB (27.32%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let k = 1;
+    let count = 1;
+    
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] === nums[i - 1]) {
+            if (count === 2) {
+                nums.splice(i , 1);
+                i--;
+            } else {
+                count++;
+                k++;
+            }
+        } else {
+            count = 1;
+            k++;
+        }
+    }
+    
+    return k;
+};
