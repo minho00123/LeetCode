f3a50628c904a3fb0ae5d9e3de25adf9c16e298b
Min Ho Jang <128145017+minho00123@users.noreply.github.com>
2024-07-21 09:57:33 +0900

    Time: 47 ms (95.45%), Space: 50.7 MB (68.76%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeOuterParentheses = function(s) {
+    let answer = "";
+    let parenthesesCount = 0;
+    
+    for (const char of s) {
+        if (char === "(") {
+            if (parenthesesCount) {
+                answer += char;
+            }
+            
+            parenthesesCount++;
+        } else {
+             parenthesesCount--;
+
+            if (parenthesesCount) {
+                answer += char;
+            }
+        }
+    }
+    
+    return answer;
+};
