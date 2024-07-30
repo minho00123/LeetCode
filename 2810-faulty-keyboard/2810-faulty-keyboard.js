e0a611f9333826b3d58b7b5501f3dff08a60f226
Min Ho Jang <128145017+minho00123@users.noreply.github.com>
2024-07-30 10:32:20 +0900

    Time: 110 ms (48.02%), Space: 53.7 MB (89.85%) - LeetHub

---
+++ 2810-faulty-keyboard/2810-faulty-keyboard.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var finalString = function(s) {
+    const arr = [];
+
+    for (const char of s) {
+        if (char === "i") {
+            arr.reverse();
+        } else {
+            arr.push(char);
+        }
+    }
+    
+    return arr.join("");
+};
